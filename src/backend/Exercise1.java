9e40eca1eb916059ee902264eede68efda8af075
bobbibao <lehoangbao1125@gmail.com>
2024-04-11 08:28:14 +0700

    Exercise 1: Implement question 1
    - Create connection to mysql in backend
    
    - Implement it on frontend

--- src/backend/Exercise1.java
+++ src/backend/Exercise1.java
@@ -1,5 +1,16 @@
 package backend;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class Exercise1 {
+	public void connectDB() throws Exception{
+		String url = "jdbc:mysql://localhost:3306/classicmodels";
+		String user = "root";
+		String password = "bobbibao";
+		Connection conn = DriverManager.getConnection(url, user, password);
+		if(conn != null) {
+			System.out.println("Connect success!");
+		}
+	}
 }
--- src/frontend/Program1.java
+++ src/frontend/Program1.java
@@ -1,4 +1,10 @@
 package frontend;
 
+import backend.Exercise1;
+
 public class Program1 {
+	public static void main(String[] args) throws Exception {
+		Exercise1 ex1 = new Exercise1();
+		ex1.connectDB();
+	}
 }
