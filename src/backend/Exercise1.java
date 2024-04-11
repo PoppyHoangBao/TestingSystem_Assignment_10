81078f286150eb7c0c21c4b2e75649a7502e088e
bobbibao <lehoangbao1125@gmail.com>
2024-04-11 20:29:15 +0700

    Exercise1: Implemented question2_getInfo

--- src/backend/Exercise1.java
+++ src/backend/Exercise1.java
@@ -2,15 +2,35 @@ package backend;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Exercise1 {
-	public void connectDB() throws Exception{
+	private static Connection conn;
+	public Exercise1() throws SQLException {
 		String url = "jdbc:mysql://localhost:3306/classicmodels";
 		String user = "root";
 		String password = "bobbibao";
-		Connection conn = DriverManager.getConnection(url, user, password);
-		if(conn != null) {
-			System.out.println("Connect success!");
+		conn = DriverManager.getConnection(url, user, password);
+	}
+	public void question1_connectDB() throws SQLException{
+		if(conn == null)
+			new Exercise1();
+		System.out.println("Connect success");
+	}
+	public static Connection getConnection() {
+		return conn;
+	}
+	
+	public void question2_getInfo() throws SQLException {
+		String sql = "select customerNumber, customerName from customers";
+		Statement statement = conn.createStatement();
+		ResultSet resultSet = statement.executeQuery(sql);
+		while(resultSet.next()) {
+			System.out.println("CustomerID: " + resultSet.getInt("customerNumber"));
+			System.out.println("CustomerName: " + resultSet.getString("customerName"));
+			System.out.println();
 		}
 	}
 }
--- src/frontend/Program1.java
+++ src/frontend/Program1.java
@@ -5,6 +5,7 @@ import backend.Exercise1;
 public class Program1 {
 	public static void main(String[] args) throws Exception {
 		Exercise1 ex1 = new Exercise1();
-		ex1.connectDB();
+		ex1.question1_connectDB();
+		ex1.question2_getInfo();
 	}
 }
